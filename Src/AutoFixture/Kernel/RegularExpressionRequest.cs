5d9802c6d518073f4d3cfed05a0e7624d44adc48
Arnaud Marant <arnaudmarant@gmail.com>
2014-03-21 18:54:16 +0100

    add multiple pattern support

--- Src/AutoFixture/Kernel/RegularExpressionRequest.cs
+++ Src/AutoFixture/Kernel/RegularExpressionRequest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ploeh.AutoFixture.Kernel
 {
@@ -7,7 +9,9 @@ namespace Ploeh.AutoFixture.Kernel
     /// </summary>
     public class RegularExpressionRequest : IEquatable<RegularExpressionRequest>
     {
-        private readonly string pattern;
+        private readonly IEnumerable<string> _patterns;
+        private readonly int? minLength;
+        private readonly int? maxLength;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RegularExpressionRequest"/> class.
@@ -20,7 +24,26 @@ namespace Ploeh.AutoFixture.Kernel
                 throw new ArgumentNullException("pattern");
             }
 
-            this.pattern = pattern;
+            this._patterns = new List<string> { pattern };
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegularExpressionRequest" /> class.
+        /// </summary>
+        /// <param name="patterns">The patterns.</param>
+        /// <param name="minLength">The minimum length.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        /// <exception cref="System.ArgumentNullException">pattern</exception>
+        public RegularExpressionRequest(IEnumerable<string> patterns, int? minLength, int? maxLength)
+        {
+            if (patterns == null)
+            {
+                throw new ArgumentNullException("pattern");
+            }
+
+            this._patterns = patterns;
+            this.minLength = minLength;
+            this.maxLength = maxLength;
         }
 
         /// <summary>
@@ -30,7 +53,46 @@ namespace Ploeh.AutoFixture.Kernel
         {
             get
             {
-                return this.pattern;
+                return this._patterns.FirstOrDefault();
+            }
+        }
+
+        /// <summary>
+        /// Gets the regular expression pattern.
+        /// </summary>
+        public IEnumerable<string> Patterns
+        {
+            get
+            {
+                return this._patterns;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the minimum length.
+        /// </summary>
+        /// <value>
+        /// The minimum length.
+        /// </value>
+        public int? MinLength
+        {
+            get
+            {
+                return minLength;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum length.
+        /// </summary>
+        /// <value>
+        /// The maximum length.
+        /// </value>
+        public int? MaxLength
+        {
+            get
+            {
+                return maxLength;
             }
         }
 
@@ -80,7 +142,9 @@ namespace Ploeh.AutoFixture.Kernel
                 return false;
             }
 
-            return this.Pattern == other.Pattern;
+            return this.Patterns == other.Patterns
+                && this.MaxLength == other.MaxLength
+                && this.minLength == other.MinLength;
         }
     }
 }
